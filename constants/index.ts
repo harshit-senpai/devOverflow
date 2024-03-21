af71a718beb7a9c1f5b1a37c40b5689777092300
harshit Mishra <miananya2@gmail.com>
2024-03-21 20:48:13 +0530

    theme switcher

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const themes = [
+    {
+        value: 'light',
+        label: 'light',
+        icon: '/assets/icons/sun.svg'
+    },
+    {
+        value: 'dark',
+        label: 'dark',
+        icon: '/assets/icons/moon.svg'
+    },
+    {
+        value: 'system',
+        label: 'system',
+        icon: '/assets/icons/computer.svg'
+    },
+]
