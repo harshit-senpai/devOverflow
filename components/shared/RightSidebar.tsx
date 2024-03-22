42d6a90a8b3d866463740780a1471a9be9af8c8a
harshit Mishra <miananya2@gmail.com>
2024-03-22 21:33:51 +0530

    left sidebar

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = () => {
+    return (
+        <section className='pt-36 px-2 h-screen background-light900_dark200 light-border sticky top-0 right-0  flex flex-col overflow-y-auto border-l p-6 shadow-light-600 dark:shadow-none max-xl:hidden lg:w-[350px] custom-scrollbar'>
+            RightSidebar
+        </section>
+    )
+}
+
+export default RightSidebar;
