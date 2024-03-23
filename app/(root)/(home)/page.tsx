a67e3d15611b99d4db725a24925406a510a3e1b2
harshit Mishra <miananya2@gmail.com>
2024-03-23 21:05:18 +0530

    No Result Found

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,55 @@
 import Filter from "@/components/shared/Filter";
 import HomeFilter from "@/components/shared/Home/HomeFilter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
- 
+
+const questions = [
+  {
+    _id: '1',
+    title: 'How to center a div',
+    tags: [
+      {
+        _id: '1',
+        name: 'HTML'
+      },
+      {
+        _id: '2',
+        name: 'CSS'
+      }
+    ],
+    author: 'Ken Senpai',
+    upvotes: 10,
+    views: 100,
+    answer:3,
+    createdAt: '2021-09-20T00:00:00.000Z'
+  },
+  {
+    _id: '2',
+    title: 'How to learn Next.js',
+    tags: [
+      {
+        _id: '1',
+        name: 'javascript'
+      },
+      {
+        _id: '2',
+        name: 'Next.hs'
+      }
+    ],
+    author: 'Ken Senpai',
+    upvotes: 10,
+    views: 100,
+    answer:3,
+    createdAt: '2021-09-20T00:00:00.000Z'
+  }
+]
+
+const numQuestions = questions.length
+
 export default function Home() {
   return (
     <>
@@ -31,6 +75,20 @@ export default function Home() {
           />
         </div>
         <HomeFilter/>
+        <div className="mt-10 flex w-full flex-col gap-6">
+          {
+            numQuestions > 0 ?
+            questions.map((question) => (
+              'QuestionCard'
+            ))
+            : <NoResult 
+              title="There's no question to show"
+              description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+              link='/ask-question'
+              linkTitle="Ask a Question"
+            />
+          }
+        </div>  
     </>
   )
 }
