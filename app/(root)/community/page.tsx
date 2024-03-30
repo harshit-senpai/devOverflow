7dbf6cd845d9ac8026715b73b1d05d032025864e
harshit Mishra <miananya2@gmail.com>
2024-03-30 13:01:17 +0530

    tags-page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -14,7 +14,7 @@ const Page = async () => {
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar
-          route="/"
+          route="/community"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search for great minds...."
