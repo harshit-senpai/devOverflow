03396cd71d8d189e83dd5be442fba7265d2c697a
harshit Mishra <miananya2@gmail.com>
2024-04-01 22:27:52 +0530

    using vote-question action

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
 import { formatNumber } from "@/utils/util";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
 interface Props {
   type: string;
   itemId: string;
   userId: string;
   upvotes: number;
-  hasUpvoted: boolean;
+  hasupVoted: boolean;
   downvotes: number;
-  hasDownvoted: boolean;
+  hasdownVoted: boolean;
   hasSaved: boolean;
 }
 
@@ -19,18 +25,55 @@ const Votes = ({
   itemId,
   userId,
   upvotes,
-  hasUpvoted,
+  hasupVoted,
   downvotes,
-  hasDownvoted,
+  hasdownVoted,
   hasSaved,
 }: Props) => {
+  const pathName = usePathname();
+  // const router = useRouter();
+
+  const handleSave = () => {};
+
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return;
+    }
+
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathName,
+        });
+      }
+      return;
+    }
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathName,
+        });
+      }
+      return;
+    }
+  };
+
   return (
     <div className="flex gap-5">
       <div className="flex-center gap-2.5">
         <div className="flex-center gap-1.5">
           <Image
             src={
-              hasUpvoted
+              hasupVoted
                 ? "/assets/icons/upvoted.svg"
                 : "/assets/icons/upvote.svg"
             }
@@ -38,7 +81,9 @@ const Votes = ({
             height={18}
             alt="upvotes"
             className="cursor-pointer"
-            onClick={() => {}}
+            onClick={() => {
+              handleVote("upvote");
+            }}
           />
           <div className="flex-center background-light700_dark400 mion-w-[18px] rounded-sm p-1">
             <p className="subtle-medium text-dark400_light900">0</p>
@@ -48,7 +93,7 @@ const Votes = ({
         <div className="flex-center gap-1.5">
           <Image
             src={
-              hasDownvoted
+              hasdownVoted
                 ? "/assets/icons/downvoted.svg"
                 : "/assets/icons/downvote.svg"
             }
@@ -56,7 +101,9 @@ const Votes = ({
             height={18}
             alt="dwonvote"
             className="cursor-pointer"
-            onClick={() => {}}
+            onClick={() => {
+              handleVote("downvote");
+            }}
           />
           <div className="flex-center background-light700_dark400 mion-w-[18px] rounded-sm p-1">
             <p className="subtle-medium text-dark400_light900">0</p>
