a1fad2ad76733a2e7906da81adfa79efb5ba1b78
harshit Mishra <miananya2@gmail.com>
2024-04-01 23:17:34 +0530

    fix

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -93,7 +93,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
 
       <AllAnswers
         questionId={result._id}
-        userId={JSON.stringify(mongoUser._id)}
+        userId={mongoUser._id}
         totalAnswers={result.answers.length}
       />
 
