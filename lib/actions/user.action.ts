cb4d863f41c2236f5b514351982b560063e336fe
harshit Mishra <miananya2@gmail.com>
2024-04-07 21:14:39 +0530

    search user based on query action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -19,10 +19,19 @@ import Answer from "@/database/answer.model";
 
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
-    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
     connectToDatabase();
+    const { searchQuery } = params;
 
-    const users = await User.find({}).sort({ createdAt: -1 });
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, "i") } },
+        { username: { $regex: new RegExp(searchQuery, "i") } },
+      ];
+    }
+
+    const users = await User.find(query).sort({ createdAt: -1 });
 
     return { users };
   } catch (error) {
